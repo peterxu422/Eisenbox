0f122f7a4c9c0657d24aad989ca79ff4ca062aa0
peterxu422 <peterxu422@gmail.com>
2015-01-22 01:24:31 -0500

    Adding angular boilerplate

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*!
+** Example App
+** Licensed under the Apache License v2.0
+** http://www.apache.org/licenses/LICENSE-2.0
+** Built by Jay Kanakiya ( @techiejayk )
+**/
+
+"use strict";
+
+var App = angular.module("example",[]);
+
+App.controller("ExmpCtrl",function  ($scope) {
+	//
+});
